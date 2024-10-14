70a1d2e359d4ffdf8e382a2cad993dcb6fc32429
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-10-14 10:34:54 -0500

    established connection between frontend and backend

--- frontend/src/pages/Product.js
+++ frontend/src/pages/Product.js
@@ -1,19 +1,23 @@
 import axios from 'axios'
 import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
 
 const Product = () => {
     //use state hook. Nothing in the squarer brackets cause it's empty in the beginning.
     //it has a current state and update state
-    const [product, setProduct] = useState([])
+    const [products, setProduct] = useState([])
     useEffect(()=>{
-        const fetchAllBooks = async()=>{
+        const fetchAllProducts = async()=>{
             try {
-                const res = await axios.get("http:localhost:8800//product")
+                const res = await axios.get("http://localhost:8800/product")
                 console.log(res)
             } catch (err) {
                 console.log(err)
             }
         }
+        //call the fetch function
+        fetchAllProducts()
         //empty brackets/empty array means it will run just once
     },[])
   return (
